eacf400959d12fc084ab953dfb3e8b19307936b5
EWLeo <leontev.ew@gmail.com>
2021-11-09 11:50:47 +0300

    First Commit

---
+++ src/main/java/ru/netology/sqr/SQLCalculate.java
@@ -0,0 +1,16 @@
+package ru.netology.sqr;
+
+public class SQLCalculate {
+    int totalCount = 0;
+
+    public int square(int minBorder, int maxBorder) {
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= minBorder && i * i <= maxBorder)
+                totalCount++;
+            else {
+            }
+        }
+        return totalCount;
+    }
+}
+
